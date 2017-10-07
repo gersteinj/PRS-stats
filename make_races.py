cb0060dfc56483eee4850b241820e61ad2620f59
gersteinj <gersteinj@gmail.com>
2017-10-06 20:10:36 -0400

    generated sample data

---
+++ make_races.py
@@ -0,0 +1,12 @@
+from random import randint
+
+racers = 20
+races = 3
+
+for x in range(races):
+    with open(f"file_{x}.txt", "w") as f:
+        f.write(','.join((f'Race{x}', 'Position', 'Laps', 'Time', 'Average', 'Best', 'Laps Leading', 'Name', 'Points')))
+        f.write('\n')
+        for y in range(racers):
+            f.write(','.join((str(y), str(y+1), str(randint(0, 35)), str(randint(15, 25)), str(randint(15, 25)), str(randint(0, 4)), f"team_{y}", str(randint(1, 50)))))
+            f.write('\n')
