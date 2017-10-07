6c7dacf70a40d4131ba8ec7b9e55790edeba4820
gersteinj <gersteinj@gmail.com>
2017-10-07 01:43:44 -0400

    created minimized version

--- make_races.py
+++ make_races.py
@@ -4,8 +4,8 @@ racers = 20
 races = 3
 
 for x in range(races):
-    with open(f"file_{x}.txt", "w") as f:
-        f.write(','.join((f'Race{x}', 'Position', 'Laps', 'Time', 'Average', 'Best', 'Laps Leading', 'Name', 'Points')))
+    with open(f"race_{x+1}.csv", "w") as f:
+        f.write(','.join((f'Race{x+1}', 'Position', 'Laps', 'Time', 'Average', 'Best', 'Laps Leading', 'Name', 'Points')))
         f.write('\n')
         for y in range(racers):
             f.write(','.join((str(y), str(y+1), str(randint(0, 35)), str(randint(15, 25)), str(randint(15, 25)), str(randint(0, 4)), f"team_{y}", str(randint(1, 50)))))
---
+++ single_race_minimal.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+import pandas as pd
+
+race_points = {1:50, 2:40, 3:36, 4:32, 5:30, 6:28, 7:26, 8:24, 9:22, 10:20, 11:18, 12:16, 13:14, 14:12, 15:10, 16:8, 17:6, 18:4, 19:2, 20:1}
+
+html_doc = open('sample-data.html', 'r')
+
+# create HTML soup
+soup = BeautifulSoup(html_doc, 'html.parser')
+
+# close html doc
+html_doc.close()
+
+# grab each table
+results = soup.find_all('table')[1]
+
+# only working with results for now
+# lap_times = soup.find_all('table')[2]
+
+# Make new soup for just the race results
+result_soup = BeautifulSoup(str(results), 'html.parser')
+
+header = [item.string for item in results.find_all('tr')[0] if item.string != '\n']
+rows = [[item.string for item in row if item.string != '\n'] for row in results.find_all('tr')]
+
+df = pd.DataFrame({'position': [row[0] for row in rows[1:]], 'points': [race_points[int(row[0])] for row in rows[1:]]}, index=[row[6] for row in rows[1:]])
+
+filename = f"{soup.title.text}.csv"
+df.to_csv(filename)
